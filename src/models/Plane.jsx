fe39116ce9b526d37f00dbc25e8480f6d6376ecb
Keshav <keshavtomar999@gmail.com>
2023-12-17 02:40:00 +0530

    working 3d homescreen

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import planeScene from '../assets/3d/plane.glb'
-import { useGLTF } from '@react-three/drei'
+import { useAnimations, useGLTF } from '@react-three/drei'
 
-const Plane = () => {
+const Plane = ({isRotating,...props}) => {
+    const ref = useRef();
     const {scene, animations} = useGLTF(planeScene)
-  return (
-    <mesh>
-      <primitive object={scene} />
-    </mesh>
-  )
+    const {actions} = useAnimations(animations,ref)
+
+    useEffect(() => {
+        if (isRotating) {
+          actions["Take 001"].play();
+        } else {
+          actions["Take 001"].stop();
+        }
+      }, [actions, isRotating]);
+    
+      return (
+        <mesh {...props} ref={ref}>
+          // use the primitive element when you want to directly embed a complex 3D
+          model or scene
+          <primitive object={scene} />
+        </mesh>
+      );
 }
 
 export default Plane
--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,10 +1,19 @@
 import { useGLTF } from "@react-three/drei"
 import skyScene from "../assets/3d/sky.glb"
+import { useRef } from "react"
+import { useFrame } from "@react-three/fiber"
 
-const Sky = () => {
+const Sky = ({isRotating}) => {
     const sky = useGLTF(skyScene)
+    const skyref = useRef();
+
+    useFrame((_,delta)=>{
+      if(isRotating){
+        skyref.current.rotation.y += 0.15 * delta
+      }
+    })
   return (
-    <mesh>
+    <mesh ref={skyref}>
       <primitive object={sky.scene} />
     </mesh>
   )
